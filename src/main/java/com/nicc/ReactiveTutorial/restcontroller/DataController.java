7bdaea3854f846bb62d7c850bb9ec079f2b1df03
nickdlc <njdlc22@gmail.com>
2025-08-10 14:28:17 -0400

    Add GET request

--- src/main/java/com/nicc/ReactiveTutorial/restcontroller/DataController.java
+++ src/main/java/com/nicc/ReactiveTutorial/restcontroller/DataController.java
@@ -3,10 +3,9 @@ package com.nicc.ReactiveTutorial.restcontroller;
 import com.nicc.ReactiveTutorial.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController()
@@ -17,6 +16,22 @@ public class DataController {
 
     @PostMapping("/customer")
     public Mono<Customer> createCustomer(@RequestBody Customer customer) {
-        return reactiveMongoTemplate.save(customer);
+        return reactiveMongoTemplate.save(customer)
+                .log();
+    }
+
+    @GetMapping("/customer/{customerId}")
+    public Mono<Customer> getCustomerById(@PathVariable String customerId) {
+        return fetchCustomerById(customerId)
+                .log();
+    }
+
+    private Mono<Customer> fetchCustomerById(String customerId) {
+        Criteria criteria = Criteria.where("id").is(customerId);
+        Query query = Query.query(criteria);
+
+        // find returns more than one result, so use findOne
+        return reactiveMongoTemplate.findOne(query, Customer.class)
+                .log();
     }
 }
